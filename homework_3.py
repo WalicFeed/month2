25ad1d6a175ad55a29cf80159487e0bb05c8c0d3
Nazar <nazar@mikunov.ru>
2026-04-05 01:53:38 +0600

    fix logic in age calculating, return introduce calls for objects

--- homework_3.py
+++ homework_3.py
@@ -12,7 +12,10 @@ class Person:
         month = int(self.__birth_date.split(".")[1])
         year = int(self.__birth_date.split(".")[2])
         birth_year = datetime(year, month, day) #почему в такой последовательности аааааааааа
-        return datetime.now().year - birth_year.year
+        if (datetime.now().month, datetime.now().day) < (birth_year.month, birth_year.day):
+            return datetime.now().year - birth_year.year - 1
+        else:
+            return datetime.now().year - birth_year.year
     @property
     def occupation(self):
         return self.__occupation
@@ -21,7 +24,7 @@ class Person:
         return self.__higher_education
     def introduce(self):
         print(f"{self.name} is my name.",end=" ")
-        print(f"I was born on {self.birth_date}.",end=" ")
+        print(f"I was born on {self.__birth_date}.",end=" ")
         print(f"{self.occupation} is my occupation.",end=" ")
         if self.higher_education:
             print("I do have a higher education.",end=" ")
@@ -65,14 +68,14 @@ class Factory:
 
 factory = Factory()
 classmate1 = factory.create(obj = "classmate",name = "Harry", birth_date = "19.01.1993", occupation = "teacher", higher_education = False, group_name = "A1")
-print(classmate1.age)
-# classmate2 = factory.create(obj = "classmate",name = "Hermione", birth_date ="23.01.2001", occupation = "student", higher_education =False, group_name = "A2")
-# friend1 = factory.create(obj="friend",name="Gabe",birth_date="23.06.1995",occupation="Gamer",higher_education=True,hobby="Games")
-# friend2 = factory.create(obj="friend",name="David",birth_date="26.07.1999",occupation="Preacher",higher_education=True,hobby="dancing :)")
-# person1 = factory.create(obj="person",name="Oliver",birth_date="19.01.1983",occupation="IT",higher_education=True)
-# person2 = factory.create(obj="person",name="Alice",birth_date="15.06.2013",occupation="child",higher_education=False)
-# for i in [classmate1, classmate2, friend1, friend2, person1, person2]:
-#     i.introduce()
-#     print("\n")
-# bfriend = factory.create(obj="bestFriend",name="Artem",birth_date="23.06.2004",occupation="Gamer",higher_education=True,hobby="Games",shared_memory="cs2")
-# bfriend.introduce()
+# print(classmate1.age) тест работы age
+classmate2 = factory.create(obj = "classmate",name = "Hermione", birth_date ="23.01.2001", occupation = "student", higher_education =False, group_name = "A2")
+friend1 = factory.create(obj="friend",name="Gabe",birth_date="23.06.1995",occupation="Gamer",higher_education=True,hobby="Games")
+friend2 = factory.create(obj="friend",name="David",birth_date="26.07.1999",occupation="Preacher",higher_education=True,hobby="dancing :)")
+person1 = factory.create(obj="person",name="Oliver",birth_date="19.01.1983",occupation="IT",higher_education=True)
+person2 = factory.create(obj="person",name="Alice",birth_date="15.06.2013",occupation="child",higher_education=False)
+for i in [classmate1, classmate2, friend1, friend2, person1, person2]:
+    i.introduce()
+    print("\n")
+bfriend = factory.create(obj="bestFriend",name="Artem",birth_date="23.06.2004",occupation="Gamer",higher_education=True,hobby="Games",shared_memory="cs2")
+bfriend.introduce()
